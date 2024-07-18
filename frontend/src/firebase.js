3e6e09af515c869588772261b620cbdf8f8cb477
muhmdshanub <muhmdshanoob@gmail.com>
2024-07-18 20:08:45 +0530

    firebase.js secret issue solved

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -11,13 +11,13 @@ import 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC2KSHiwBdIPR6qBeRwoIvHnRjgeIyxj8I",
-  authDomain: "vergno-38150.firebaseapp.com",
-  projectId: "vergno-38150",
-  storageBucket: "vergno-38150.appspot.com",
-  messagingSenderId: "1047052609842",
-  appId: "1:1047052609842:web:b8e48eb8bb5b6b94198f2b",
-  measurementId: "G-83E1XEBW5S"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
